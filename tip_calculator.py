b6a4636cb48f8f18453977dde589bf4ce060a63d
Antonio King Hunt <tesseractcode@icloud.com>
2023-10-26 18:26:42 -0600

    Fix: final output for total and tip instead of tip only

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,15 +1,16 @@
 import ipdb
 
 def calculate_tip(total_bill, percentage_tip, number_of_people):
-  total_tip = int(total_bill) * (0.01 * float(percentage_tip))
-  individual_tip = round(total_tip / int(number_of_people), 2)
-  return round(individual_tip, 2)
+  total_tip = float(total_bill) * (0.01 * int(percentage_tip))
+  total_bill = float(total_bill) + total_tip
+  individual_bill = round(total_bill / int(number_of_people), 2)
+  return individual_bill
 
 end = False
 
 while not end:
   print("Welcome to the tip calculator!")
-  total_bill = input("What was the total bill?\n")[1:]
+  total_bill = input("What was the total bill?\n$")
   percentage_tip = input("What percentage of tip would you like to give? 10, 12, or 15?\n")
   number_of_people = input("How many people to split the bill?\n")
   tip = calculate_tip(total_bill, percentage_tip, number_of_people)
